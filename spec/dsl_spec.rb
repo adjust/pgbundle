92c71e9584a4cc3ac11075ca1aa3efa328052917
Manuel Kniep <m.kniep@web.de>
2014-06-17 14:04:09 +0200

    adds port option to the database

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -4,5 +4,6 @@ describe PgBundle::Dsl do
 
   subject { PgBundle::Dsl.new.eval_pgfile(File.expand_path('../Pgfile', __FILE__)) }
   its(:database) { should be_a PgBundle::Database }
+  its('database.port') { should be 54321 }
   its(:extensions) { should be_a Hash }
 end
